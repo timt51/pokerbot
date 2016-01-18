7bc55622fcb67c61e369430c122524b2354cfdf7
Timothy F Truong <ttruong@mit.edu>
2016-01-17 19:09:34 -0500

    first commit

---
+++ d/Player.py
@@ -0,0 +1,147 @@
+import argparse
+import socket
+import sys
+import newhand, getaction
+from deuces import Evaluator
+import os
+import time
+import cPickle
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+import lasagne
+
+def build_cnn(input_var=None):
+    # As a third model, we'll create a CNN of two convolution + pooling stages
+    # and a fully-connected hidden layer in front of the output layer.
+
+    # Input layer, as usual:
+    network = lasagne.layers.InputLayer(shape=(None, 39, 17, 17),
+                                        input_var=input_var)
+    # This time we do not apply input dropout, as it tends to work less well
+    # for convolutional layers.
+
+    # Convolutional layer with 32 kernels of size 5x5. Strided and padded
+    # convolutions are supported as well; see the docstring.
+    network = lasagne.layers.Conv2DLayer(
+            network, num_filters=32, filter_size=(5, 5),
+            nonlinearity=lasagne.nonlinearities.rectify,
+            W=lasagne.init.GlorotUniform())
+    # Expert note: Lasagne provides alternative convolutional layers that
+    # override Theano's choice of which implementation to use; for details
+    # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
+
+    # Max-pooling layer of factor 2 in both dimensions:
+    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+
+    # Another convolution with 32 5x5 kernels, and another 2x2 pooling:
+    network = lasagne.layers.Conv2DLayer(
+            network, num_filters=32, filter_size=(5, 5),
+            nonlinearity=lasagne.nonlinearities.rectify)
+    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+
+    # A fully-connected layer of 256 units with 50% dropout on its inputs:
+    network = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(network, p=.5),
+            num_units=256,
+            nonlinearity=lasagne.nonlinearities.rectify)
+
+    # And, finally, the 10-unit output layer with 50% dropout on its inputs:
+    network = lasagne.layers.DenseLayer(
+            lasagne.layers.dropout(network, p=.5),
+            num_units=5,
+            nonlinearity=lasagne.nonlinearities.softmax)
+
+    return network
+    
+"""
+Simple example pokerbot, written in python.
+
+This is an example of a bare bones pokerbot. It only sets up the socket
+necessary to connect with the engine and then always returns the same action.
+It is meant as an example of how a pokerbot should communicate with the engine.
+"""
+class Player:
+    def run(self, input_socket, evaluator, network):
+        # Get a file-object for reading packets from the socket.
+        # Using this ensures that you get exactly one packet per read.
+        f_in = input_socket.makefile()
+        while True:
+            # Block until the engine sends us a packet.
+            data = f_in.readline().strip().split()
+            # If data is None, connection has closed.
+            if not data:
+                print "Gameover, engine disconnected."
+                break
+
+            # Here is where you should implement code to parse the packets from
+            # the engine and act on it. We are just printing it instead.
+            print data, "\n"
+
+            # When appropriate, reply to the engine with a legal action.
+            # The engine will ignore all spurious responses.
+            # The engine will also check/fold for you if you return an
+            # illegal action.
+            # When sending responses, terminate each response with a newline
+            # character (\n) or your bot will hang!
+            word = data[0]
+            if word == "GETACTION":
+                # Currently CHECK on every move. You'll want to change this.
+                getaction.move(data,oppName,holeCards,button,s,evaluator,network)
+            elif word == "NEWHAND":
+                handID = data[1]
+                button = bool(data[2])
+                holeCards = data[3:7]
+                myBank = data[7]
+                otherBank = data[8]
+                timeBank = data[9]
+            elif word == "NEWGAME":
+                myName = data[1]
+                oppName = data[2]
+                stackSize = data[3]
+                bb = data[4]
+                numHands = data[5]
+                timeBank = data[6]
+            elif word == "HANDOVER":
+                myBankRoll = data[1]
+                oppBankRoll = data[2]
+                numBoardCards = int(data[3])
+                boardCards = data[4:4 + numBoardCards]
+                numLastActions = int(data[4 + numBoardCards])
+                lastActions = data[5 + numLastActions:-1]
+                timeBank = data[-1]
+            elif word == "REQUESTKEYVALUES":
+                # At the end, the engine will allow your bot save key/value pairs.
+                # Send FINISH to indicate you're done.
+                s.send("FINISH\n")
+        # Clean up the socket.
+        s.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A Pokerbot.', add_help=False, prog='pokerbot')
+    parser.add_argument('-h', dest='host', type=str, default='localhost', help='Host to connect to, defaults to localhost')
+    parser.add_argument('port', metavar='PORT', type=int, help='Port on host to connect to')
+    args = parser.parse_args()
+
+    # Create a socket connection to the engine.
+    print 'Connecting to %s:%d' % (args.host, args.port)
+    try:
+        s = socket.create_connection((args.host, args.port))
+    except socket.error as e:
+        print 'Error connecting! Aborting'
+        exit()
+
+    with open('network.pickle') as f:
+        all_param_values = cPickle.load(f)
+    input_var = T.tensor4('inputs')
+    network = build_cnn(input_var)
+    lasagne.layers.set_all_param_values(network, all_param_values)
+    
+    bot = Player()
+    evaluator = Evaluator()
+    bot.run(s, evaluator, network)
+
+
+#beginning strategy?
---
+++ d/getaction.py
@@ -0,0 +1,127 @@
+import itertools
+from deuces import Card, Deck #7462 max
+import time
+import random
+
+def move(data,oppName,holeCards,button,s,evaluator,network):
+	potSize = data[1]
+	numBoardCards = int(data[2])
+	boardCards = data[3:3 + numBoardCards]
+	numLastActions = int(data[3 + numBoardCards])
+	lastActions = data[4 + numBoardCards:4 + numBoardCards + numLastActions]
+	numLegalActions = int(data[4 + numBoardCards + numLastActions])
+	legalActions = data[5 + numBoardCards + numLastActions:-1]
+	timebank = data[-1]
+
+	avg_rank = average_rank(evaluator, holeCards, boardCards)
+	call_odds = odds(oppName, potSize, lastActions)
+
+	if "RAISE" in legalActions[-1] or "BET" in legalActions[-1]:
+		max_raise = legalActions[-1].split(':')[-1]
+		raise_odds = int(max_raise) / (int(potSize) + int(max_raise) + 0.0)
+
+		print "raise odds", raise_odds, (1- avg_rank/7462) + 0.4
+
+		if (1- avg_rank/7462) - random.random()/3 > raise_odds:
+			if "RAISE" in legalActions[-1]:
+				s.send("RAISE:" + max_raise + "\n")
+			elif "BET" in legalActions[-1]:
+				s.send("BET:" + max_raise + "\n")
+		else:
+			s.send("CALL\n")
+	elif (1 - avg_rank/7462) - random.random()/5 > call_odds:
+			s.send("CALL\n")
+	else:
+			s.send("FOLD\n")
+
+	# if "POST" in "".join(lastActions):
+	# 	if button == True:
+	# 		if avg_rank/7462 < 0.95:
+	# 			s.send("CALL\n")
+	# 		else:
+	# 			s.send("FOLD\n")
+	# 	else:
+	# 		if avg_rank/7462 < 0.50:
+	# 			s.send("CALL\n")
+	# 		else:
+	# 			s.send("FOLD\n")
+	# else:
+	# 	if button == True:
+	# 		if avg_rank/7462 - 0.1 > call_odds:
+	# 			s.send("CALL\n")
+	# 		else:
+	# 			s.send("FOLD\n")
+	# 	else:
+	# 		if avg_rank/7462  - 0.1 > call_odds:
+	# 			s.send("CALL\n")
+	# 		else:
+	# 			s.send("FOLD\n")
+
+def rank(evaluator, holeCards, boardCards):
+	deck = Deck()
+
+	allCards = holeCards.union(boardCards)
+	while len(allCards) < 9:
+		card = deck.draw(1)
+		if len(allCards) != len(allCards.union([card])):
+			allCards.add(card)
+			boardCards.add(card)
+
+	all2CardCombos = itertools.combinations(holeCards,2)
+	all3CardCombos = itertools.combinations(boardCards,3)
+
+	min_rank = 10000
+
+	for handCombo in all2CardCombos:
+		for boardCombo in all3CardCombos:
+			rank = evaluator.evaluate(boardCombo, handCombo)
+			if rank < min_rank:
+				min_rank = rank
+
+	return min_rank
+
+def average_rank(evaluator, holeCards, boardCards):
+	holeCards = map(Card.new, holeCards)
+	boardCards = map(Card.new, boardCards)
+
+	if len(boardCards) == 0 or len(boardCards) == 3:
+		reps = 101
+	elif len(boardCards) == 4:
+		reps = 51
+	else:
+		reps = 2
+
+	ranks = [0.0]
+	for i in range(1,reps):
+		ranks.append(rank(evaluator, set(holeCards), set(boardCards)))
+
+	return sum(ranks)/(len(ranks)-1)
+
+def odds(oppName, potSize, lastActions):
+	#either post, bet, raise, check
+	#only care about opponent moves
+	lastActions = [action for action in lastActions if oppName in action]
+	if len(lastActions) == 0:
+		return 0
+	lastAction = lastActions[-1]
+
+	if "CHECK" in lastAction:
+		call = 0
+	elif "BET" in lastAction:
+		call = int(lastAction[4])
+	elif "RAISE" in lastAction:
+		call = (int(potSize)-int(lastAction[6]))
+	elif "POST" in lastAction:
+		call = 1
+	else:
+		call = 0
+
+	return call/(float(potSize)+call)
+
+#aggression index
+#if aggression < __:
+#check/fold
+#elif aggression < __:
+#call
+#else
+#bet/raise
