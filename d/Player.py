ac39be4882a48e925ef26b62193e7924636d0097
Timothy F Truong <ttruong@mit.edu>
2016-01-18 12:43:04 -0500

    argmax

--- d/Player.py
+++ d/Player.py
@@ -151,7 +151,7 @@ class Player:
                         else:
                             river_cnt += 1
 
-                prediction = predict_fn(data_tensor)
+                prediction = predict_fn(T.argmax(data_tensor))
                 print(prediction)
 
                 s.send("CALL\n")
