db20ca3489dd8421e8d3b3802b588c0ea123e655
Timothy Truong Jr <timt51@live.com>
2016-01-19 14:42:11 -0500

    add theano and lasagne locally

---
+++ d/theano/generated_version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM THEANO SETUP.PY
+short_version = '0.7.0'
+version = '0.7.0'
+git_revision = '54186290a97186b9c6b76317e007844529a352f4'
+full_version = '0.7.0.dev-%(git_revision)s' % {
+    'git_revision': git_revision}
+release = False
+
+if not release:
+    version = full_version
